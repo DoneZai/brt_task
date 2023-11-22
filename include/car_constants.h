e1b0c28b01445b57a1778f305611abe8ee53c896
Jinlong Gongye <j_gongye@baumanracing.ru>
2023-11-22 13:32:48 +0300

    header file

--- include/car_constants.h
+++ include/car_constants.h
@@ -4,15 +4,16 @@
 
 namespace CarConstants {
    
-    constexpr float m=300;
-    constexpr float Iz=134;
-    constexpr float lf=0.721;
-    constexpr float lr=0.823;
-    constexpr float Cm=3600;
-    constexpr float Crr=200;
-    constexpr float Cd=1.53;
-    constexpr float Cbf=5411;
-    constexpr float Cbr=2650;
+    constexpr float m = 300;
+    constexpr float Iz = 134;
+    constexpr float b = 0.669;
+    constexpr float lf = 0.721;
+    constexpr float lr = 0.823;
+    constexpr float Cm = 3600;
+    constexpr float Crr = 200;
+    constexpr float Cd = 1.53;
+    constexpr float Cbf = 5411;
+    constexpr float Cbr = 2650;
     constexpr float Iwz = 0.24;
 
 }  
